d705a630332ebb8fdfe7db6cbad6a692d7fb5764
EveningstarSona <iamsonaeveningstar@gmail.com>
2020-10-28 18:17:05 -0300

    Uploading current work

---
+++ GAMSECOMP 2020/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private FieldOfView fieldOfView;
+
+    private float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        //mousePosition = Input.mousePosition;
+        //fieldOfView.SetAimDirection()
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
