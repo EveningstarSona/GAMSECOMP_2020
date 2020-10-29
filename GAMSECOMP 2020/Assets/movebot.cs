4aea08b34d86471e89bc04edc79ace570eaf8d2f
25jao <joao.costa@estudante.ufscar.br>
2020-10-29 17:54:52 -0300

    pathfinder

---
+++ GAMSECOMP 2020/Assets/movebot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movebot : MonoBehaviour
+{
+    public GameObject targetpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetpos.transform.SetParent(null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, targetpos.transform.position) < 0.1f){
+            float newposx = Random.Range(-6.1f, 28.0f);
+            float newposy = Random.Range(6.6f, 23.0f);
+            targetpos.transform.position = new Vector3(newposx, newposy, 0);
+        }
+    }
+}
